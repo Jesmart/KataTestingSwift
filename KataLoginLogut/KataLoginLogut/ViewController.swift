907f48aa706b1c733bcbeb7ac281f2a59d669ed4
jesus.martinez <jesus.martinez.lopez@bbva.com>
2018-09-20 15:28:46 +0200

    first commit

---
+++ KataLoginLogut/KataLoginLogut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KataLoginLogut
+//
+//  Created by jesus.martinez on 20/9/18.
+//  Copyright © 2018 JML. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
